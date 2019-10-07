4a82c9e590dc8246b9d3a7717708295da85d4986
Ryan Thorn <ryandthorn@gmail.com>
2019-10-07 12:27:43 -0700

    Server returns JSON fib sequence

--- server/main.go
+++ server/main.go
@@ -1,22 +1,49 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/julienschmidt/httprouter"
 )
 
-func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	fmt.Fprint(w, "Welcome!\n")
+func fib(n int) int {
+	a := 0
+	b := 1
+	for i := 0; i < n; i++ {
+		temp := a
+		a = b
+		b = temp + a
+	}
+	return a
 }
-func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
+
+// Respond with a JSON array of a Fibonacci sequence
+func Fibonacci(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	// Get value from URL params
+	digits := ps.ByName("digits")
+
+	// Convert string to integer
+	i, err := strconv.Atoi(digits)
+	if err != nil {
+		log.Println("Conversion error")
+		return
+	}
+
+	var sequence []int
+	for n := 0; n < i; n++ {
+		// Compute number.
+		result := fib(n)
+		sequence = append(sequence, result)
+	}
+
+	json.NewEncoder(w).Encode(sequence)
 }
+
 func main() {
 	router := httprouter.New()
-	router.GET("/api", Index)
-	router.GET("/api/hello/:name", Hello)
+	router.GET("/api/fibonacci/:digits", Fibonacci)
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
